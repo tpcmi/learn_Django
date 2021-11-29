82cfa26879c637613ca7fbd3e37c9440a7923afe
tpcmi <724801800@qq.com>
2021-11-29 08:58:47 +0800

    。

--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self) -> str:
         return self.question_text
